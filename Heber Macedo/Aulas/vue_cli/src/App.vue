18aebed0f760986ca2ca89d4d0ed6099084b80e5
Heber Americo Macedo <heber.macedo@dinfor.uniceub>
2026-04-01 21:35:44 -0300

    alteração

--- Heber Macedo/Aulas/vue_cli/src/App.vue
+++ Heber Macedo/Aulas/vue_cli/src/App.vue
@@ -1,20 +1,37 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Meu primeiro App Vue</h1>
+    <p class="subtitulo">Vamos aprender muito!</p>
+    <br>
+    <estilo-component />
+    <br>
     <home-component/>
     <hr>
-    <home-component/>
+    <usuario-component />
   </div>
- 
 </template>
 
 <script>
 import HomeComponent from './components/HomeComponent.vue';
+import UsuarioComponent from './components/UsuarioComponent.vue';
+import EstiloComponent from './components/EstiloComponent.vue';
 export default {
   name: 'App',
   components: {
-    HomeComponent, 
-  },
+    HomeComponent,
+    UsuarioComponent,
+    EstiloComponent,
+},
+  mounted() {
+    console.log("mounted");
+},
+errorCaptured() {
+    console.log("errorCaptured");
+},
+  created() {
+    console.log("created");
+},
 };
 </script>
 
