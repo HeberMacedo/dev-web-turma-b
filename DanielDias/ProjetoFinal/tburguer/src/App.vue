ee8843732d44d8fd5468f5ffffbcfd380ae39e5b
Danieldiaxf <danieldias0312@gmail.com>
2026-06-25 21:13:26 -0300

    Feat: Melhorando o Design Interno

--- DanielDias/ProjetoFinal/tburguer/src/App.vue
+++ DanielDias/ProjetoFinal/tburguer/src/App.vue
@@ -19,17 +19,25 @@ export default {
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #faf8f4;
+  margin: 0;
+}
+
 #app {
   font-family: Georgia, 'Times New Roman', Times, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
-
-nav a {
-  color: #f5e6c8;
+  color: #2c2c2c;
 }
 
 nav a.router-link-exact-active {
   color: #d4a847;
+  border-bottom: 2px solid #d4a847;
+  padding-bottom: 2px;
 }
 </style>
